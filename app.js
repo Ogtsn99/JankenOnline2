658e7252effcdf9092bbd41af25fcae8aadd708e
Ogtsn99 <fantasista4123@icloud.com>
2020-05-26 03:00:09 +0900

    init

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var helmet = require('helmet');
+var session = require('express-session');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +26,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(session({
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport-twitterの設定
+passport.use(new TwitterStrategy({
+  consumerKey: 'RpwO4HmlMAF8KeHnXdu9IyD5A',
+  consumerSecret: 'mdvqn6OaWGqw9FB5oDdWD4qCYMxItSq4tQH0WqL0w1QefkZYqF',
+  callbackURL: '..../auth/twitter/callback'
+},
+// 認証後の処理
+function(token, tokenSecret, profile, done) {
+  return done(null, profile);
+}
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/?auth_failed' }),
+  function (req, res) {
+    res.redirect('/success');
+  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
