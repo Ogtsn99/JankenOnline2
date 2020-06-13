e132e3475a5831a070c42599516a71ce47435552
Ogtsn99 <fantasista4123@icloud.com>
2020-06-13 11:19:26 +0900

    edit

--- app/entry.js
+++ app/entry.js
@@ -1,1 +1,2 @@
 'use strict';
+import $ from 'jquery';
--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,37 @@ router.get('/:userId', function(req, res, next) {
     else{
       Result.findByPk(userId).then(result=>{
         if(!result) res.send("ごめんね。対戦成績が見つからなかったよ");
-        else res.render('user', {user: user, result: result, yours: true});
+        else res.render('user', {user: user, result: result, url: req.url});
       })
     }
   })
 });
 
+router.get('/:userId/edit', function(req, res, next) {
+  var userId = req.params.userId.slice(1);
+  User.findByPk(userId).then(user => {
+    if(!user) res.send("ごめんね。ユーザーが見つからなかったよ");
+    else{
+      if(user.userTwitterId != req.user.id.toString()){
+        res.send("権限がありません");
+      }else res.render('edit', {user: user});
+    }
+  })
+});
+
+router.post('/:userId', (req, res) => {
+  var userId = req.params.userId.slice(1);
+  User.findByPk(userId).then(user => {
+    if(!user) res.send("ごめんね。ユーザーが見つからなかったよ");
+    else{
+      if(user.userTwitterId != req.user.id.toString()){
+        res.send("権限がありません");
+      }else {
+        const newName = req.body.name; //newnameに変更してupdate
+        user.username = newName;
+        User.update(user);
+      }
+    }
+  })
+})
 module.exports = router;
