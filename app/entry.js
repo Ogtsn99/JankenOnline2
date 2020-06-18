ff084fd5dafbe49828fb35ee5447a949ccda23c6
Ogtsn99 <fantasista4123@icloud.com>
2020-06-18 17:08:48 +0900

    多分バグがたくさんだと思いますが

--- app/entry.js
+++ app/entry.js
@@ -4,22 +4,30 @@ const global = Function('return this;')();
 global.jQuery = $;
 
 //import io from 'socket.io-client';
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 
-
-
-const btnGu = $('#btnGu');
-const btnPa = $('#btnPa');
-const btnChoki = $('#btnChoki');
+var start = false;
+var btnGu = $('#btnGu');
+var btnPa = $('#btnPa');
+var btnChoki = $('#btnChoki');
+var opponentName = $('#opponentName');
+var nextTe = $('#nextTe');
 
 btnGu.click(()=>{
-    
+    socket.emit("janken_to_server", {te: 0});
+    nextTe.text("次に出す手: グー")
 })
 btnPa.click(()=>{
-    
+    socket.emit("janken_to_server", {te: 1});
+    nextTe.text("次に出す手: チョキ")
 })
 btnChoki.click(()=>{
-    
+    socket.emit("janken_to_server", {te: 2});
+    nextTe.text("次に出す手: パー")
 })
 
+//対戦開始、相手の情報(名前)を受け取る
+function setOpponentName(name){
+    opponentName.text(name);
+}
 
--- routes/janken.js
+++ routes/janken.js
@@ -5,35 +5,42 @@ const Result = require('../models/result');
 
 router.get('/:roomId', (req, res) => {
     var roomId = req.params.roomId;
-    /* 
-        TODO 同じルームに同じユーザーがいるのは弾く
-    */
-    if(roomId.length >= 5) {
+    if(!req.isAuthenticated()){
+        res.send("先にログインをお願いします")
+    }else if(roomId.length >= 5) {
         res.send("部屋がありませんでした");
         res.end();
     }else{
-
-        console.log(roomId + "への接続を試みました");
-        //変更が必要
-        userId = Math.random().toString(32).substring(2);
-    //    userId = req.user.id; if(!userId) res.send("先にログインをお願いします");
-        var sz = countPeople[roomId];
-        if(typeof(sz) === 'undefined') sz = 0;
-        if(sz <= 1){
-            console.log("接続できたよ!");
-            countPeople[roomId]++;
-            roomMember[roomId][sz] = userId;
-            res.render('janken', {roomId: roomId});
-        }else{
-            res.send("部屋に入れませんでした。おそらく人数がいっぱいです。")
-        }
+        var userId = req.user.id;
+        User.findOne({
+                where: {userTwitterId: userId}
+            }
+        ).then((user) => {
+            var username = user.username;
+            var sz = countPeople[roomId];
+            if(typeof(sz) === 'undefined') sz = 0;
+            if(sz === 1){
+                if(roomMember[roomId][0].userId === userId){
+                    res.send("部屋に入れませんでした。")
+                }else{
+                    //roomMember[roomId][sz] = {userId: userId, username: username, socketId: null};
+                    res.render('janken', {roomId: roomId, username: username, userId: userId});
+                }
+            }else if(sz === 0){
+                roomMember[roomId][0] = {userId: null, username: null, socketId: null};
+                roomMember[roomId][1] = {userId: null, username: null, socketId: null};
+                res.render('janken', {roomId: roomId, username: username, userId: userId});
+            }else{
+                res.send("部屋に入れませんでした。人数がいっぱい、もしくは更新中です。")
+            }
+        })
     }
 });
 /*
 global.numToJanken = {0: "グー", 1: "チョキ", 2: "パー"};
 global.te = {}; //userId: ユーザーの次の手
 global.countPeople = {}; //roomId: ルームにいるユーザーの数
-global.roomMember = {}; //roomId: ルームにいるユーザー1のID, ユーザー2のID
+global.roomMember = {}; //roomId: ルームにいるユーザー1の(userid, username, socketId), ユーザー2のID
 global.userResult = {}; //userId: 勝ち, あいこ, 負け, gu, choki, pa
 */
 module.exports = router;
